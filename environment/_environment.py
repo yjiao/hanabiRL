5c33f6829ad7889d31e60786c9bb2be9e7bc3cd7
Joy Jiao <jjiao-mbp@Jjiao-mbp.local>
2018-09-27 00:21:21 -0700

    environment: add card and environment basics

--- environment/_environment.py
+++ environment/_environment.py
@@ -1,2 +1,36 @@
+from . import card
+
+
 class Environment(object):
-    pass
+    def __init__(self, num_players):
+        self.reset(num_players)
+
+    def reset(self, num_players):
+        self.players = [card.Pile(card.PileType.PLAYER)] * num_players
+        self.num_players = num_players
+        self.discard = card.Pile(card.PileType.DISCARD)
+        self.deck = card.Pile(card.PileType.DECK)
+        self.board = card.Pile(card.PileType.BOARD)
+        self.hints = card.MAX_HINTS
+        self.bombs = 0
+
+        for i in range(card.MIN_CARD_VALUE, card.MAX_CARD_VALUE + 1):
+            for color in card.Color:
+                if color == card.Color.UNK:
+                    continue
+                self.deck.add_card(card.Card(color=color, value=i))
+
+    def __repr__(self):
+        return f"""
+==============================================
+Environment
+----------------------------------------------
+hints remaining: {self.hints}
+bombs: {self.bombs}
+num_players: {self.num_players}
+players: {self.players}
+discard: {self.discard}
+deck: {self.deck}
+board: {self.board}
+==============================================
+        """
--- environment/card.py
+++ environment/card.py
@@ -1,5 +1,47 @@
 from collections import namedtuple
-Card = namedtuple('Card', ['color', 'number'])
+from enum import IntEnum
+
+MAX_CARD_VALUE = 5
+MIN_CARD_VALUE = 1
+MAX_HINTS = 8
+MAX_BOMBS = 3
+
+
+Card = namedtuple("Card", ["color", "value"])
+
+
+class Color(IntEnum):
+    UNK = 0
+    BLUE = 1
+    GREEN = 2
+    RED = 3
+    WHITE = 4
+    YELLOW = 5
+
+
+class PileType(IntEnum):
+    DISCARD = 1
+    DECK = 2
+    PLAYER = 3
+    BOARD = 4
+
 
 class Pile(object):
-    pass
+    def __init__(self, pile_type):
+        self.type = pile_type
+        self.cards = set()
+
+    def __repr__(self):
+        rep = f"pile of type {self.type.name} ({self.type}) with {len(self.cards)} cards\n"
+        for card in sorted(self.cards):
+            rep += f"    card: {card.color.name: >8}, {card.value: >3}\n"
+        return rep
+
+    def add_card(self, card):
+        self.cards.add(card)
+
+    def remove_card(self, card):
+        if card in self.cards:
+            self.cards.remove(card)
+        else:
+            raise Exception(f"Card {card} is not in deck {self.type}.name.")
