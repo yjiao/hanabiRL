40f17f8aca11cc0ffeeec57b14b5c374c3c8a158
Joy Jiao <jjiao-mbp@Jjiao-mbp.local>
2018-09-27 00:56:32 -0700

    environment: remove num_players as a param from reset(), lint

--- environment/_environment.py
+++ environment/_environment.py
@@ -1,19 +1,21 @@
 from . import card
 
+
 class Environment(object):
     def __init__(self, num_players):
-        self.reset(num_players)
-
-    def reset(self, num_players):
-        self.players = [card.Pile(card.PileType.PLAYER)] * num_players
         self.num_players = num_players
+        self.reset()
+
+    def reset(self):
+        """Reset environment to start of a game."""
+        self.players = [card.Pile(card.PileType.PLAYER)] * self.num_players
         self.discard = card.Pile(card.PileType.DISCARD)
         self.deck = card.Pile(card.PileType.DECK)
         self.board = card.Pile(card.PileType.BOARD)
         self.hints = card.MAX_HINTS
         self.bombs = 0
         self.state = []
-        self.feature_idx = {}
+        self.feature_idx = {}  # this keeps track of where each feature is
 
         for i in range(card.MIN_CARD_VALUE, card.MAX_CARD_VALUE + 1):
             for color in card.Color:
@@ -33,24 +35,24 @@ class Environment(object):
         players
         """
         state = []
-        self.feature_idx['hints'] = len(state)
+        self.feature_idx["hints"] = len(state)
         state.append(self.hints)
 
-        self.feature_idx['bombs'] = len(state)
+        self.feature_idx["bombs"] = len(state)
         state.append(self.bombs)
 
-        self.feature_idx['bombs'] = len(state)
+        self.feature_idx["bombs"] = len(state)
         state.extend(self.board.to_list())
 
-        self.feature_idx['deck'] = len(state)
+        self.feature_idx["deck"] = len(state)
         state.extend(self.deck.to_list())
 
-        self.feature_idx['discard'] = len(state)
+        self.feature_idx["discard"] = len(state)
         state.extend(self.discard.to_list())
 
-        self.feature_idx['players'] = []
+        self.feature_idx["players"] = []
         for player in self.players:
-            self.feature_idx['players'].append(len(state))
+            self.feature_idx["players"].append(len(state))
             state.extend(player.to_list())
 
         self.state = state
