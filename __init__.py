7a23a2e304cb897fca816947fdef91618bdf2c5b
Joy Jiao <jjiao-mbp@Jjiao-mbp.local>
2018-09-26 23:41:41 -0700

    Added stub files

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from . import Environment
+from . import State
+from . import Pile
+from . import Card
---
+++ card.py
@@ -0,0 +1,5 @@
+from collections import namedtuple
+Card = namedtuple('Card', ['color', 'number'])
+
+class Pile(object):
+    pass
